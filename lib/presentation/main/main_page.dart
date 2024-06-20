81973f76b1f548c42d1d83bfb7215f485c2f25f6
imnadev <imnadev@gmail.com>
2024-06-20 20:00:14 +0500

    Fixed cubit bug

--- lib/presentation/main/main_page.dart
+++ lib/presentation/main/main_page.dart
@@ -1,18 +1,15 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:hrplus/common/base/base_page.dart';
 import 'package:hrplus/common/extensions/theme_extensions.dart';
 import 'package:hrplus/common/gen/assets.gen.dart';
 import 'package:hrplus/common/router/app_router.dart';
-import 'package:hrplus/presentation/main/cubit/main_cubit.dart';
-import 'package:hrplus/presentation/main/cubit/main_state.dart';
 
 @RoutePage()
-class MainPage extends BasePage<MainCubit, MainBuildable, MainListenable> {
+class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
-  Widget builder(BuildContext context, MainBuildable state) {
+  Widget build(BuildContext context) {
     return AutoTabsScaffold(
       routes: const [
         HomeRoute(),
